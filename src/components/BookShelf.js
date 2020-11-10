372fb0210d29a559a2db0741f01eca4e9e1263ab
magicmarie <natukunda162@gmail.com>
2020-11-10 23:05:37 +0300

    feature: update the book and bookshelf components

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,18 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 import Book from './Book';
 
 class BookShelf extends React.Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  }
+
   render() {
-    const { shelfName } = this.props;
+    const { shelfName, books } = this.props;
+    console.log(books);
+    console.log(shelfName);
+    console.log(books.filter(book => console.log(book)));
+
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{shelfName}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book/>
-            </li>
+            {books.filter(book => book.shelf === {shelfName}).map((book) =>
+              <Book key={book.id} book={book} />
+            )}
           </ol>
         </div>
       </div>
